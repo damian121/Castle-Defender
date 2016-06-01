1ff1c0cc1e41046ed09a1ec18b80a16c04ab61b4
Ruben Alkemade <Ruben Alkemade>
2016-06-01 11:34:50 +0200

    Added knight.js

---
+++ knight.js
@@ -0,0 +1,9 @@
+function Knight() {
+    this.pos = width + 50;
+    this.speedMod = 1 + getRandomInt(-20, 50) / 100;
+    this.legProgress = 0;
+
+    this.update() {
+        this.pos -= 2 * this.speedMod;
+    }
+}
