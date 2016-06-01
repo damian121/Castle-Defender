75b179935549320fc3050ccf4d30fef066b3c4ec
Julian <minecraftmacjuul@gmail.com>
2016-06-01 10:12:31 +0200

    Added charging animation

--- main.js
+++ main.js
@@ -6,13 +6,20 @@ var canvas = document.getElementById('cvs'),
     running = true,
     width = 1250,
     height = 620,
-    imgs = ["grond", "sky", "sky_cloud_1", "sky_cloud_2", "sky_cloud_3", "wall", "barrel", "wheel"],
+    imgs = ["grond", "sky", "sky_cloud_1", "sky_cloud_2", "sky_cloud_3", "wall", "barrel", "charge", "wheel"],
     sky_1_offset = 0,
     sky_2_offset = 0,
     mouse = [null, null],
     sky_3_offset = 0,
     tower_offset = getRandomInt(0, 100),
+    bal = null,
+    charging = false,
+    mayCharge = true,
+    chargeLoc = [null, null],
+    chargePower = 80,
+    chargeState = false,
     images = {},
+    ball = null,
     barrelRotation = 0,
     muteBtn = document.getElementById('btn'),
     music = new Audio('music.mp3');
@@ -42,7 +49,6 @@ function tick() {
 }
 
 function update() {
-
     sky_1_offset -= 0.8;
     sky_2_offset -= 0.4;
     sky_3_offset -= 0.2;
@@ -60,19 +66,36 @@ function update() {
     }
 
     if(mouse[0]) {
-        let cannon = [45 + images.barrel.width / 2, 200 + images.barrel.height / 2 + tower_offset],
-            angle = getAngle(cannon, [mouse[0], mouse[1] + 27]);
+        let cannon = [images.barrel.width / 2 - 31, 170 + images.barrel.height / 2 + tower_offset],
+            angle = getAngle(cannon, [mouse[0], mouse[1]]);
 
-        if(angle > 0) {
-            angle = 0;
+        if(angle > 20) {
+            angle = 20;
         }
 
-        if(angle <= -90) {
-            angle = -90;
+        if(angle <= -65) {
+            angle = -65;
         }
 
         barrelRotation = angle;
     }
+
+    // Charging arrow
+    if(chargeState) {
+        chargePower += 0.8;
+    } else {
+        chargePower -= 0.8;
+    }
+
+    // Min 68% - Max 90%
+    if(chargePower < 60 || chargePower > 90) {
+        chargeState = !chargeState;
+    }
+
+    let x = 45 + (1035 * (chargePower / 900)) * Math.cos(barrelRotation * Math.PI / 180);
+    let y = (199 + tower_offset) + (1035 * (chargePower / 900)) * Math.sin(barrelRotation * Math.PI / 180);
+
+    chargeLoc = [x, y];
 }
 
 function render() {
@@ -88,6 +111,11 @@ function render() {
     gfx.drawImage(images.sky_cloud_3, sky_3_offset, 0, width, height);
     gfx.drawImage(images.sky_cloud_3, width + sky_3_offset, 0, width, height);
 
+    // charge
+    if(charging && chargeLoc[0]) {
+        drawRotatedImage(images.charge, barrelRotation, chargeLoc[0], chargeLoc[1], (images.charge.width / 2) * (chargePower / 100), (images.charge.height / 2) * (chargePower / 100));
+    }
+
     // Cannon
     drawRotatedImage(images.barrel, barrelRotation, 45, 200 + tower_offset);
     gfx.drawImage(images.wheel, 25, 197 + tower_offset);
@@ -100,6 +128,8 @@ function render() {
     gfx.drawImage(images.grond, images.grond.width, 550);
     gfx.drawImage(images.grond, images.grond.width * 2, 550);
     gfx.drawImage(images.grond, images.grond.width * 3, 550);
+
+    // Debug
 }
 
 // Start our game
@@ -147,9 +177,31 @@ function drawRotatedImage(image, angle, x, y, width, height) {
 	gfx.restore();
 }
 
-document.addEventListener('mousemove', function(e) {
-    mouse = [e.clientX, e.clientY];
-})
+cvs.addEventListener('mousemove', function(evt) {
+    let rect = cvs.getBoundingClientRect(),
+        root = document.documentElement;
+
+    // return relative mouse position
+    let mouseX = evt.clientX - rect.left - root.scrollLeft;
+    let mouseY = evt.clientY - rect.top - root.scrollTop;
+
+    mouse = [mouseX, mouseY];
+});
+
+cvs.addEventListener('mousedown', function(evt) {
+    if(mayCharge) {
+        if(!charging) {
+            chargePower = 60;
+        }
+
+        charging = true;
+        mayCharge = false;
+    }
+});
+
+cvs.addEventListener('mouseup', function(evt) {
+    charging = false;
+});
 
 function getAngle(pos1, pos2) {
     let angle = Math.atan2(pos2[1] - pos1[1], pos2[0] - pos1[0]) * (180/Math.PI);
