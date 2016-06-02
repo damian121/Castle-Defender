a75c7233299b01bdf8cfc8ae7e5576f55cfc1285
Julian <minecraftmacjuul@gmail.com>
2016-06-02 13:47:49 +0200

    Added a crap load of new stuff and fixes

--- knight.js
+++ knight.js
@@ -1,10 +1,12 @@
 function Knight() {
     this.pos = width + 50;
-    this.speedMod = 1 + getRandomInt(-20, 50) / 100;
+    this.speedMod = 1 + getRandomInt(-20, 80) / 100;
     this.legProgress = 0;
     this.legState = true;
     this.alive = true;
     this.deathPos = 0;
+    this.deathFade = 2;
+    this.knightType = getRandomInt(1, 5);
     this.deathVel;
 
     this.update = function() {
@@ -23,6 +25,12 @@ function Knight() {
 
     this.dead = function() {
         this.deathVel += 0.2;
+        this.deathFade -= 0.04;
+
+        if(this.deathFade < 0) {
+            this.deathFade = 0;
+        }
+
         this.deathPos += this.deathVel;
     }
 }
--- main.js
+++ main.js
@@ -5,14 +5,24 @@ var canvas = document.getElementById('cvs'),
     gfx = canvas.getContext('2d'),
     running = true,
     width = 1250,
+    endFade = 0,
     height = 620,
     spawnSpeed = 1500,
-    imgs = ["grond", "sky", "sky_cloud_1", "knight", "leg_1", "leg_2", "sky_cloud_2", "sky_cloud_3", "wall", "ball", "barrel", "charge", "wheel"],
+    imgs = ["grond", "meeuw", "meeuw_vleugels", "egg",
+    "sky", "bord", "warning", "bord_mini", "sky_cloud_1",
+    "menu", "knight1", "knight2", "knight3", "knight4",
+    "knight5", "leg_1", "leg_2", "sky_cloud_2", "sky_cloud_3",
+    "wall", "ball", "barrel", "charge", "wheel"],
     sky_1_offset = 0,
     sky_2_offset = 0,
+    meeuw = null,
+    egg = [null, null],
     mouse = [null, null],
     sky_3_offset = 0,
-    tower_offset = getRandomInt(0, 100),
+    score = 0,
+    highscore = 0,
+    scoreObj = document.getElementById('score'),
+    tower_offset = getRandomInt(0, 150),
     knights = [],
     bal = null,
     charging = false,
@@ -24,7 +34,12 @@ var canvas = document.getElementById('cvs'),
     ball = null,
     barrelRotation = 0,
     muteBtn = document.getElementById('btn'),
-    music = new Audio('music.mp3');
+    music = new Audio('music.mp3'),
+    boom1 = new Audio('boom1.mp3'),
+    boom2 = new Audio('boom2.mp3'),
+    boom3 = new Audio('boom3.mp3'),
+    boom4 = new Audio('boom4.mp3'),
+    mute = false;
 
 music.loop = true;
 music.play();
@@ -47,6 +62,8 @@ function tick() {
 
     if(running) {
         window.requestAnimationFrame(tick);
+    } else {
+        finish();
     }
 }
 
@@ -71,8 +88,8 @@ function update() {
         let cannon = [images.barrel.width / 2 - 31, 170 + images.barrel.height / 2 + tower_offset],
             angle = getAngle(cannon, [mouse[0], mouse[1]]);
 
-        if(angle > 20) {
-            angle = 20;
+        if(angle > 30) {
+            angle = 30;
         }
 
         if(angle <= -65) {
@@ -101,7 +118,7 @@ function update() {
 
     // Cannonball
     if(ball) {
-        if(ball.pos[1] > 550) {
+        if(ball.pos[1] > 550 || ball.pos[0] > width + 10) {
             ball = null;
             mayCharge = true;
         } else {
@@ -116,21 +133,27 @@ function update() {
     }
 
     // Knights
-    knights.forEach(function(knight) {
+    knights.forEach(function(knight, i) {
         if(knight.alive) {
             knight.update();
         } else {
             knight.dead();
+
+            if(knight.deathVel > 7) {
+                knights.splice(i, 1);
+            }
         }
 
         if(knight.pos <= 50) {
-            // running = false;
+            running = false;
         }
 
-        // 480
-        if(ball && getDistance(ball.pos, [knight.pos, 500]) <= 56) {
+        if(ball && getDistance(ball.pos, [knight.pos, 500]) <= 56 && knight.alive) {
             knight.alive = false;
             knight.deathVel = -2.3;
+
+            score++;
+            scoreObj.innerHTML = score;
         }
 
         // Increase the spawn speed
@@ -140,6 +163,25 @@ function update() {
             spawnSpeed = 400;
         }
     });
+
+    // Meeuw
+    if(meeuw && !meeuw.dead) {
+        meeuw.update();
+
+        if(meeuw.pos < -100) {
+            meeuw = null;
+        } else {
+            if(ball && !meeuw.dead && getDistance(ball.pos, [meeuw.pos, meeuw.hoogte]) < 22) {
+                meeuw.dead = true;
+
+                egg = [meeuw.pos + 22, meeuw.hoogte + 6];
+            }
+        }
+    }
+
+    if(egg[0]) {
+        egg[1] += 4;
+    }
 }
 
 function render() {
@@ -171,9 +213,22 @@ function render() {
 
     // knights
     knights.forEach(function(knight) {
+        if(!knight.alive) {
+            gfx.globalAlpha = knight.deathFade;
+        }
+
+        let type = images['knight' + knight.knightType];
+
         drawRotatedImage(images.leg_2, knight.legProgress, knight.pos + 22, 532 + knight.deathPos);
         drawRotatedImage(images.leg_1, knight.legProgress - (knight.legProgress * 2), knight.pos + 22, 532 + knight.deathPos);
-        gfx.drawImage(images.knight, knight.pos, 458 + knight.deathPos);
+        gfx.drawImage(type, knight.pos, 458 + knight.deathPos);
+
+        if(!knight.alive) {
+            gfx.globalAlpha = knight.deathFade;
+            gfx.drawImage(images.warning, knight.pos + 15, 382 + knight.deathFade * 10, images.warning.width * 0.8,  images.warning.height * 0.8);
+        }
+
+        gfx.globalAlpha = 1;
     });
 
     // Tower
@@ -184,6 +239,23 @@ function render() {
     gfx.drawImage(images.grond, images.grond.width, 550);
     gfx.drawImage(images.grond, images.grond.width * 2, 550);
     gfx.drawImage(images.grond, images.grond.width * 3, 550);
+
+    // egg
+    if(egg[0]) {
+        gfx.drawImage(images.egg, egg[0], egg[1]);
+    }
+
+    // Meeuw
+    if(meeuw && !meeuw.dead) {
+        gfx.drawImage(images.meeuw, meeuw.pos, meeuw.hoogte);
+        drawRotatedImage(images.meeuw_vleugels, meeuw.rot, meeuw.pos + 39, meeuw.hoogte + 13);
+    }
+
+    // Score Board
+    gfx.drawImage(images.bord, 20, 20, images.bord.width * 0.9, images.bord.height * 0.9);
+
+    // Sound baord
+    gfx.drawImage(images.bord_mini, width - images.bord_mini.width - 20, 20, images.bord_mini.width * 0.9, images.bord_mini.height * 0.9);
 }
 
 // Start our game
@@ -208,6 +280,21 @@ function shoot() {
         vx: vx,
         amp: amp
     }
+
+    if(!mute) {
+        let sound;
+
+        switch(getRandomInt(1, 4)) {
+        case 1: sound = boom1; break;
+        case 2: sound = boom2; break;
+        case 3: sound = boom3; break;
+        case 4: sound = boom4; break;
+        }
+
+        sound.load();
+        sound.volume = 0.4;
+        sound.play();
+    }
 }
 
 document.getElementById('container').style.width = cvs.clientWidth + "px";
@@ -219,10 +306,12 @@ muteBtn.addEventListener("click", function() {
     if(tog == "true") {
         muteBtn.src = "img/mute.png";
         music.volume = 0;
+        mute = true;
         muteBtn.setAttribute('data-enabled', 'false');
     } else {
         muteBtn.src = "img/sound.png";
         music.volume = 1;
+        mute = false;
         muteBtn.setAttribute('data-enabled', 'true');
     }
 });
@@ -316,9 +405,15 @@ function getAngle(pos1, pos2) {
 }
 
 function spawn() {
-    let knight = new Knight();
+    if(running) {
+        let knight = new Knight();
 
-    knights.push(knight);
+        knights.push(knight);
+
+        if(!meeuw && getRandomInt(1, 5) == 1) {
+            meeuw = new Meeuw();
+        }
+    }
 
     setTimeout(function() {
         spawn();
@@ -330,3 +425,58 @@ spawn();
 
 // Show the canvas
 document.getElementById('container').style.opacity = 1;
+
+function finish() {
+    let menu = document.getElementById('menu'),
+        opacity = 0;
+
+    if(score > highscore) {
+        highscore = score;
+        document.getElementById('menu-high-score').innerHTML = highscore;
+    }
+
+    menu.style.display = "block";
+
+    document.getElementById('menu-score').innerHTML = score;
+
+    let itv = setInterval(function() {
+        opacity += 0.05;
+
+        if(opacity >= 1) {
+            opacity = 1;
+            clearInterval(itv);
+        }
+
+        menu.style.opacity = opacity;
+    }, 10);
+}
+
+function restart() {
+    knights = [];
+    spawnSpeed = 1500;
+    ball = null;
+    score = 0;
+    mayCharge = true;
+    tower_offset = getRandomInt(0, 150);
+
+    scoreObj.innerHTML = 0;
+
+    let opacity = 1;
+
+    let itv = setInterval(function() {
+        opacity -= 0.05;
+
+        if(opacity <= 0) {
+            opacity = 0;
+            menu.style.display = "none";
+            document.getElementById('menu-score').innerHTML = 0;
+            clearInterval(itv);
+        }
+
+        menu.style.opacity = opacity;
+    }, 10);
+
+    running = true;
+
+    tick();
+}
